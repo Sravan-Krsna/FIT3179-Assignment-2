2ccf5805d8fef850649af05caa2602f64e15b482
Sravan-Krsna <88082746+Sravan-Krsna@users.noreply.github.com>
2021-10-15 18:00:52 +0800

    edited datasets

--- Data/data.py
+++ Data/data.py
@@ -2,9 +2,11 @@ import pandas as pd
 
 data_2021 = pd.read_csv("Data/index2021_data.csv")
 data_2019 = pd.read_csv("Data/index2019_data.csv")
-print(data_2021.columns)
-unemployment = data_2021[['Country Name', 'Unemployment (%)']]
-unemployment_2019 = data_2019[['Country Name','Unemployment (%)']]
+#print(data_2021.columns)
+#unemployment = data_2021[['Country Name', 'Unemployment (%)']]
+#unemployment_2019 = data_2019[['Country Name','Unemployment (%)']]
 #unemployment.rename(columns={'Unemployment (%)':'2021'}, inplace=True)
-unemployment.to_csv("unemployment2021.csv")
-unemployment_2019.to_csv("unemployment2019.csv")
+data = pd.read_csv("Data/unemployment2019.csv")
+data2 = pd.read_csv("Data/Unemployment2021.csv")
+data = data.append(data2, ignore_index=True)
+data.to_csv("unemployment.csv")
