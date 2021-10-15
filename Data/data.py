205745dd8d25871a2f711f36cc6cf84625f94167
Sravan-Krsna <88082746+Sravan-Krsna@users.noreply.github.com>
2021-10-15 17:43:56 +0800

    changed datasets

--- Data/data.py
+++ Data/data.py
@@ -4,6 +4,7 @@ data_2021 = pd.read_csv("Data/index2021_data.csv")
 data_2019 = pd.read_csv("Data/index2019_data.csv")
 print(data_2021.columns)
 unemployment = data_2021[['Country Name', 'Unemployment (%)']]
-unemployment['2019'] = data_2019['Unemployment (%)']
-unemployment.rename(columns={'Unemployment (%)':'2021'}, inplace=True)
-unemployment.to_csv("unemployment.csv")
+unemployment_2019 = data_2019[['Country Name','Unemployment (%)']]
+#unemployment.rename(columns={'Unemployment (%)':'2021'}, inplace=True)
+unemployment.to_csv("unemployment2021.csv")
+unemployment_2019.to_csv("unemployment2019.csv")
